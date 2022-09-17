af4f7372a198f95254923b912bb26235cf1ff172
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-09-16 22:20:25 -0500

    global values

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var form = document.getElementById('passwordform');
+var length = document.getElementById('length');
+var numeric = document.getElementById('numeric');
+var special = document.getElementById('special');
+var uppercase = document.getElementById('uppercase');
+var lowercase = document.getElementById('lowercase');
 
 // Write password to the #password input
 function writePassword() {
