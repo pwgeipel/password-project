72aa5096022197a07e0f8b137a86fc0effc14f0a
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-09-18 14:30:07 -0500

    keys

--- script.js
+++ script.js
@@ -2,11 +2,21 @@
 var generateBtn = document.querySelector("#generate");
 var form = document.getElementById('passwordform');
 var length = document.getElementById('length');
-var numeric = document.getElementById('numeric');
-var special = document.getElementById('special');
-var uppercase = document.getElementById('uppercase');
-var lowercase = document.getElementById('lowercase');
+var numeric = document.getElementById('numeric').checked;
+var special = document.getElementById('special').checked;
+var uppercase = document.getElementById('uppercase').checked;
+var lowercase = document.getElementById('lowercase').checked;
+if (numeric + special + uppercase + lowercase === 0) {
+  alert("Select at least one option.");
+  return;
+}
 
+var keys = {
+  numeric: "0123456789",
+  special: "!@#$%^&*()_+~\\`|}{[]:;?><,./-=",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowercase: "abcdefghijklmnopqrstuvwxyz"
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -18,3 +28,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
