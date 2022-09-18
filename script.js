e5ef794c7241fc9e86ec388735fde6df31d6b66f
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-09-18 17:32:27 -0500

    writePassword

--- script.js
+++ script.js
@@ -2,14 +2,9 @@
 var generateBtn = document.querySelector("#generate");
 var form = document.getElementById('passwordform');
 var length = document.getElementById('length');
-var numeric = document.getElementById('numeric').checked;
-var special = document.getElementById('special').checked;
-var uppercase = document.getElementById('uppercase').checked;
-var lowercase = document.getElementById('lowercase').checked;
-if (numeric + special + uppercase + lowercase === 0) {
-  alert("Select at least one option.");
-  return;
-}
+
+
+
 
 var keys = {
   numeric: "0123456789",
@@ -27,11 +22,20 @@ var getChar = [
     keys.uppercase[Math.floor(Math.random() * keys.uppercase.length)];
   function lowercase() {
     keys.lowercase[Math.floor(Math.random() * keys.lowercase.length)];  
-]
+  }
+];
 
 
 // Write password to the #password input
 function writePassword() {
+  var numeric = document.getElementById('numeric').checked;
+  var special = document.getElementById('special').checked;
+  var uppercase = document.getElementById('uppercase').checked;
+  var lowercase = document.getElementById('lowercase').checked;
+  if (numeric + special + uppercase + lowercase === 0) {
+  alert("Select at least one option.");
+  return;
+  }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
