3f73a6b2e0332dfbaa11e9f2c55672dda7145221
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-09-19 16:42:37 -0500

    character arrays

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // // Assignment Code
-// var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 // var form = document.getElementById('passwordform');
 
 
 
 
 
-var keys = {
-  numeric: "0123456789",
-  special: "!@#$%^&*_-+=",
-  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  lowercase: "abcdefghijklmnopqrstuvwxyz"
+
+var numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialArray = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "="];
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 }
 
 var getChar = [
