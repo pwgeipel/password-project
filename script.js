2ef5a47ad39c0d5a1d7eb29fc0b632e97090ef1a
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-09-19 21:54:31 -0500

    special char

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialArray = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "="];
+var specialArray = [" ", "!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "=", "(", ")", "<", ">", "?"];
 var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var resultArray = [];
